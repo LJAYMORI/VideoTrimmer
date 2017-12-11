f3a1ff41a5e5e7702d18886ea1fc479b090bf49c
JongukLee <ljay.mori@gmail.com>
2017-12-11 15:44:01 +0900

    Add simple-exo-player for playing video by uri

--- app/src/main/java/com/jonguk/videotrimmer/MainActivity.kt
+++ app/src/main/java/com/jonguk/videotrimmer/MainActivity.kt
@@ -1,56 +1,86 @@
 package com.jonguk.videotrimmer
 
-import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.view.SurfaceView
 import android.view.View
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
+import android.widget.Toast
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.BehindLiveWindowException
 import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.TrackGroupArray
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.trackselection.TrackSelection
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
-import com.jakewharton.rxbinding2.view.RxView
-import com.tbruyelle.rxpermissions2.RxPermissions
+import com.google.android.exoplayer2.util.Util
+import com.jonguk.videotrimmer.exo.EventLogger
+import com.jonguk.videotrimmer.exo.PlayerEventListener
+import java.net.CookieHandler
+import java.net.CookieManager
+import java.net.CookiePolicy
 
 class MainActivity : AppCompatActivity() {
+    private val loadView by lazy { findViewById<View>(R.id.load_button) }
+    private val videoView by lazy { findViewById<SimpleExoPlayerView>(R.id.exo_player_view) }
+    private val mediaControlView by lazy { findViewById<MediaControllerView>(R.id.media_controller_view) }
+
+    private var mainHandler: Handler? = null
+    private var eventLogger: EventLogger? = null
+    private var player: SimpleExoPlayer? = null
+    private var mediaDataSourceFactory: DataSource.Factory? = null
+    private var trackSelector: DefaultTrackSelector? = null
+    private var lastSeenTrackGroupArray: TrackGroupArray? = null
+
+    private var shouldAutoPlay: Boolean = false
+    private var resumeWindow: Int? = 0
+    private var resumePosition: Long? = 0L
 
     companion object {
         @JvmStatic val REQ_CODE_MAIN = 123
+
+        private val defaultCookieManager: CookieManager by lazy { CookieManager() }
+        private val BANDWIDTH_METER = DefaultBandwidthMeter()
     }
 
-    private val loadView by lazy { findViewById<View>(R.id.load_button) }
-    private val videoLayout by lazy { findViewById<AspectRatioFrameLayout>(R.id.video_layout) }
-    private val videoSurfaceView by lazy { findViewById<SurfaceView>(R.id.video_surface_view) }
-    private val mediaControlView by lazy { findViewById<MediaControllerView>(R.id.media_controller_view) }
-
-    private val player: SimpleExoPlayer =
-            ExoPlayerFactory.newSimpleInstance(DefaultRenderersFactory(this),
-                    DefaultTrackSelector(), DefaultLoadControl())
+    init {
+        defaultCookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ORIGINAL_SERVER)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        shouldAutoPlay = true
+        clearResumePosition()
+        mediaDataSourceFactory = DefaultDataSourceFactory(this, BANDWIDTH_METER,
+                DefaultHttpDataSourceFactory(Util.getUserAgent(this, application.packageName),
+                        BANDWIDTH_METER))
+        mainHandler = Handler()
+        val currentHandler = CookieHandler.getDefault()
+        if (currentHandler != defaultCookieManager) {
+            CookieHandler.setDefault(defaultCookieManager)
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initializePlayer()
-
-        val disposable = RxView.clicks(loadView)
-                .compose(RxPermissions(this).ensure(Manifest.permission.READ_EXTERNAL_STORAGE))
-                .subscribe({ granted ->
-                    if (granted) {
-//                        startActivityForResult(VideoGalleryActivity.newIntent(this), REQ_CODE_MAIN)
-                    } else {
-                        // TODO
-                    }
-                }, {})
+        videoView.requestFocus()
+//        val disposable = RxView.clicks(loadView)
+//                .compose(RxPermissions(this).ensure(Manifest.permission.READ_EXTERNAL_STORAGE))
+//                .subscribe({
+//                    startActivityForResult(VideoGalleryActivity.newIntent(this), REQ_CODE_MAIN)
+//                }, {})
     }
 
     override fun onStart() {
@@ -81,54 +111,146 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        releasePlayer()
+        shouldAutoPlay = true
+        clearResumePosition()
+        setIntent(intent)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (data != null && resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 REQ_CODE_MAIN -> {
                     val uriPath = data.extras?.getString(Constant.VIDEO_URI)
-                    if (uriPath != null) {
-                        player.prepare(buildMediaSource(Uri.parse(uriPath)), true, false)
+                    uriPath?.let {
+                        player?.prepare(buildMediaSource(Uri.parse(it)), true, false)
                     }
                 }
             }
         }
     }
 
+    private fun buildMediaSource(uri: Uri): MediaSource = ExtractorMediaSource(uri,
+            mediaDataSourceFactory, DefaultExtractorsFactory(), mainHandler, eventLogger)
+
     private fun initializePlayer() {
-        player.playWhenReady = true
-        player.seekTo(0)
+        // TODO local video
+//        var intent: Intent? = intent
+
+        var playerEventListener: PlayerEventListener? = null
+        val needNewPlayer = player == null
+        if (needNewPlayer) {
+            val adaptiveTrackSelection: TrackSelection.Factory =
+                    AdaptiveTrackSelection.Factory(BANDWIDTH_METER)
+            trackSelector = DefaultTrackSelector(adaptiveTrackSelection)
+            lastSeenTrackGroupArray = null
+            eventLogger = EventLogger(trackSelector)
+
+
+//            player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector)
+            playerEventListener = PlayerEventListener.Builder()
+                    .trackSelector(trackSelector)
+                    .lastSeenTrackGroupArray(lastSeenTrackGroupArray)
+                    .updateResumePositionCallback { updateResumePosition() }
+                    .showControlCallback { showControls() }
+                    .updateButtonVisibilitiesCallback { updateButtonVisibilities() }
+                    .initializePlayerCallback { initializePlayer() }
+                    .clearResumePositionCallback { clearResumePosition() }
+                    .isBehindLiveWindowCallback { isBehindLiveWindow(it) }
+                    .messageCallback { resId, arg -> showToast(resId, arg) }
+                    .build()
+            player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+            player?.addListener(playerEventListener)
+            player?.addListener(eventLogger)
+            player?.addMetadataOutput(eventLogger)
+            player?.setAudioDebugListener(eventLogger)
+            player?.setVideoDebugListener(eventLogger)
+
+            videoView.player = player
+            player?.playWhenReady = shouldAutoPlay
+
+        }
+
+        val haveResumePosition = resumeWindow != C.INDEX_UNSET
+        if (haveResumePosition) {
+            player?.seekTo(resumeWindow ?: 0, resumePosition ?: 0)
+        }
 
         val uri = Uri.parse("http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_1mb.mp4")
-        player.prepare(buildMediaSource(uri), true, false)
+        player?.prepare(buildMediaSource(uri), !haveResumePosition, false)
+        playerEventListener?.setErrorState(false)
+        updateButtonVisibilities()
     }
 
     private fun releasePlayer() {
-        player.let { player.release() }
-//        if (player != null) {
-//            playbackPosition = player.getCurrentPosition();
-//            currentWindow = player.getCurrentWindowIndex();
-//            playWhenReady = player.getPlayWhenReady();
-//            player.release();
-//            player = null;
-//        }
-    }
-
-    private fun hideSystemUi() {
-        var visibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            visibility = (visibility or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        player?.let {
+            shouldAutoPlay = it.playWhenReady
+            updateResumePosition()
+            it.release()
+            player = null
+            trackSelector = null
+            eventLogger = null
         }
     }
 
-    private fun buildMediaSource(uri: Uri) = ExtractorMediaSource(
-            uri, DefaultHttpDataSourceFactory("ua"),
-            DefaultExtractorsFactory(),
-            null, null)
+    private fun updateResumePosition() {
+        resumeWindow = player?.currentWindowIndex
+        resumePosition = Math.max(0L, player?.contentPosition ?: 0L)
+    }
+
+    private fun showControls() {
+        mediaControlView?.visibility = View.VISIBLE
+    }
+
+    private fun updateButtonVisibilities() {
+        player?.let {
+            val mappedTrackInfo = trackSelector?.currentMappedTrackInfo
+            mappedTrackInfo?.let {
+                for (i in 0 until it.length) {
+                    val groups = it.getTrackGroups(i)
+                    if (groups.length != 0) {
+                        val label: Int = when (player?.getRendererType(i)) {
+                            C.TRACK_TYPE_AUDIO -> R.string.audio
+                            C.TRACK_TYPE_VIDEO -> R.string.video
+                            C.TRACK_TYPE_TEXT -> R.string.text
+                            else -> 0
+                        }
+                        if (label != 0) {
+                            // TODO something..
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun clearResumePosition() {
+        resumeWindow = C.INDEX_UNSET
+        resumePosition = C.TIME_UNSET
+    }
+
+    private fun isBehindLiveWindow(e: ExoPlaybackException?) : Boolean {
+        if (e?.type != ExoPlaybackException.TYPE_SOURCE) {
+            return false
+        }
+        var cause: Throwable? = e.sourceException
+        while (cause != null) {
+            if (cause is BehindLiveWindowException) {
+                return true
+            }
+            cause = cause.cause
+        }
+        return false
+    }
+
+    private fun showToast(resId: Int, arg: Any?) {
+        if (arg != null) {
+            Toast.makeText(this, getString(resId, arg), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
---
+++ app/src/main/java/com/jonguk/videotrimmer/exo/PlayerEventListener.java
@@ -0,0 +1,242 @@
+package com.jonguk.videotrimmer.exo;
+
+import android.support.annotation.StringRes;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.mediacodec.MediaCodecRenderer;
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.jonguk.videotrimmer.R;
+
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.functions.Function;
+
+
+public class PlayerEventListener extends Player.DefaultEventListener {
+
+    private DefaultTrackSelector trackSelector;
+    private TrackGroupArray lastSeenTrackGroupArray;
+
+    private boolean inErrorState;
+    private Runnable updateResumePositionCallback;
+    private Runnable showControlCallback;
+    private Runnable updateButtonVisibilitiesCallback;
+    private Runnable initializePlayerCallback;
+    private Runnable clearResumePositionCallback;
+    private Function<ExoPlaybackException, Boolean> isBehindLiveWindowCallback;
+    private BiConsumer<Integer, Object> messageCallback;
+
+    private PlayerEventListener() {}
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == Player.STATE_ENDED) {
+            showControls();
+        }
+
+        updateResumePosition();
+    }
+
+    @Override
+    public void onPositionDiscontinuity(@Player.DiscontinuityReason int reason) {
+        if (inErrorState) {
+            // This will only occur if the user has performed a seek whilst in the error state. Update
+            // the resume position so that if the user then retries, playback will resume from the
+            // position to which they seeked.
+            updateResumePosition();
+        }
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException e) {
+        if (e.type == ExoPlaybackException.TYPE_RENDERER) {
+            Exception cause = e.getRendererException();
+            if (cause instanceof MediaCodecRenderer.DecoderInitializationException) {
+                // Special case for decoder initialization failures.
+                MediaCodecRenderer.DecoderInitializationException decoderInitializationException =
+                        (MediaCodecRenderer.DecoderInitializationException) cause;
+                if (decoderInitializationException.decoderName == null) {
+                    if (decoderInitializationException.getCause() instanceof MediaCodecUtil.DecoderQueryException) {
+                        messageCallback(R.string.error_querying_decoders, null);
+                    } else if (decoderInitializationException.secureDecoderRequired) {
+                        messageCallback(R.string.error_no_secure_decoder,
+                                decoderInitializationException.mimeType);
+                    } else {
+                        messageCallback(R.string.error_no_decoder,
+                                decoderInitializationException.mimeType);
+                    }
+                } else {
+                    messageCallback(R.string.error_instantiating_decoder,
+                            decoderInitializationException.decoderName);
+                }
+            }
+        }
+
+        inErrorState = true;
+        if (isBehindLiveWindowCallback(e)) {
+            clearResumePosition();
+            initializePlayer();
+        } else {
+            updateResumePosition();
+            updateButtonVisibilities();
+            showControls();
+        }
+    }
+
+    @Override
+    @SuppressWarnings("ReferenceEquality")
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+        updateButtonVisibilities();
+        if (trackGroups != lastSeenTrackGroupArray) {
+            MappingTrackSelector.MappedTrackInfo mappedTrackInfo = trackSelector.getCurrentMappedTrackInfo();
+            if (mappedTrackInfo != null) {
+                if (mappedTrackInfo.getTrackTypeRendererSupport(C.TRACK_TYPE_VIDEO)
+                        == MappingTrackSelector.MappedTrackInfo.RENDERER_SUPPORT_UNSUPPORTED_TRACKS) {
+                    messageCallback(R.string.error_unsupported_video, null);
+                }
+                if (mappedTrackInfo.getTrackTypeRendererSupport(C.TRACK_TYPE_AUDIO)
+                        == MappingTrackSelector.MappedTrackInfo.RENDERER_SUPPORT_UNSUPPORTED_TRACKS) {
+                    messageCallback(R.string.error_unsupported_audio, null);
+                }
+            }
+            lastSeenTrackGroupArray = trackGroups;
+        }
+    }
+
+    public void setErrorState(boolean state) {
+        inErrorState = state;
+    }
+
+    public boolean isInErrorState() {
+        return inErrorState;
+    }
+
+    private void showControls() {
+        if (showControlCallback != null) {
+            showControlCallback.run();
+        }
+    }
+
+    private void updateResumePosition() {
+        if (updateResumePositionCallback != null) {
+            updateResumePositionCallback.run();
+        }
+    }
+
+    private void updateButtonVisibilities() {
+        if (updateButtonVisibilitiesCallback != null) {
+            updateButtonVisibilitiesCallback.run();
+        }
+    }
+
+    private void messageCallback(@StringRes int resId, Object args) {
+        if (messageCallback != null) {
+            try {
+                messageCallback.accept(resId, args);
+            } catch (Exception ignore) {}
+        }
+    }
+
+    private boolean isBehindLiveWindowCallback(ExoPlaybackException e) {
+        if (isBehindLiveWindowCallback != null) {
+            try {
+                return isBehindLiveWindowCallback.apply(e);
+            } catch (Exception ignore) {}
+        }
+        return false;
+    }
+
+    private void clearResumePosition() {
+        if (clearResumePositionCallback != null) {
+            clearResumePositionCallback.run();
+        }
+    }
+
+    private void initializePlayer() {
+        if (initializePlayerCallback != null) {
+            initializePlayerCallback.run();
+        }
+    }
+
+    public static class Builder {
+        private DefaultTrackSelector trackSelector;
+        private TrackGroupArray lastSeenTrackGroupArray;
+
+        private Runnable updateResumePositionCallback;
+        private Runnable showControlCallback;
+        private Runnable updateButtonVisibilitiesCallback;
+        private Runnable initializePlayerCallback;
+        private Runnable clearResumePositionCallback;
+        private Function<ExoPlaybackException, Boolean> isBehindLiveWindowCallback;
+        private BiConsumer<Integer, Object> messageCallback;
+
+        public Builder trackSelector(DefaultTrackSelector trackSelector) {
+            this.trackSelector = trackSelector;
+            return this;
+        }
+
+        public Builder lastSeenTrackGroupArray(TrackGroupArray lastSeenTrackGroupArray) {
+            this.lastSeenTrackGroupArray = lastSeenTrackGroupArray;
+            return this;
+        }
+
+        public Builder updateResumePositionCallback(Runnable updateResumePositionCallback) {
+            this.updateResumePositionCallback = updateResumePositionCallback;
+            return this;
+        }
+
+        public Builder showControlCallback(Runnable showControlCallback) {
+            this.showControlCallback = showControlCallback;
+            return this;
+        }
+
+        public Builder updateButtonVisibilitiesCallback(Runnable updateButtonVisibilitiesCallback) {
+            this.updateButtonVisibilitiesCallback = updateButtonVisibilitiesCallback;
+            return this;
+        }
+
+        public Builder initializePlayerCallback(Runnable initializePlayerCallback) {
+            this.initializePlayerCallback = initializePlayerCallback;
+            return this;
+        }
+
+        public Builder clearResumePositionCallback(Runnable clearResumePositionCallback) {
+            this.clearResumePositionCallback = clearResumePositionCallback;
+            return this;
+        }
+
+        public Builder isBehindLiveWindowCallback(Function<ExoPlaybackException, Boolean> isBehindLiveWindowCallback) {
+            this.isBehindLiveWindowCallback = isBehindLiveWindowCallback;
+            return this;
+        }
+
+        public Builder messageCallback(BiConsumer<Integer, Object> messageCallback) {
+            this.messageCallback = messageCallback;
+            return this;
+        }
+
+        public PlayerEventListener build() {
+            if (trackSelector == null) {
+                throw new IllegalArgumentException("trackSelector should be initialized");
+            }
+
+            PlayerEventListener listener = new PlayerEventListener();
+            listener.trackSelector = trackSelector;
+            listener.lastSeenTrackGroupArray = lastSeenTrackGroupArray;
+            listener.updateResumePositionCallback = updateResumePositionCallback;
+            listener.showControlCallback = showControlCallback;
+            listener.updateButtonVisibilitiesCallback = updateButtonVisibilitiesCallback;
+            listener.initializePlayerCallback = initializePlayerCallback;
+            listener.clearResumePositionCallback = clearResumePositionCallback;
+            listener.isBehindLiveWindowCallback = isBehindLiveWindowCallback;
+            listener.messageCallback = messageCallback;
+            return listener;
+        }
+    }
+
+}
